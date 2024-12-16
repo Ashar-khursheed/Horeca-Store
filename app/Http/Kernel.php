53fa7b97cefb4124f4362f71c32aef7a0334843a
Ashar-khursheed <asharkhursheed26@gmail.com>
2024-12-16 15:53:26 +0500

    commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\Cors::class,
         
     ];
 
